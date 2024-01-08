b0ee74197f6f8f9f55a42228ef3de37a75ed613a
ahosan_habib <ahosanhabib310@gmail.com>
2024-01-08 19:17:39 +0600

    html & js complete

---
+++ script.js
@@ -0,0 +1,30 @@
+function calculate(){
+var income =parseFloat(document.getElementById("inc").value);
+var officeRent =parseFloat(document.getElementById("oRent").value);
+var employeeSalary =parseFloat(document.getElementById("eSlary").value);
+var houseRent =parseFloat(document.getElementById("hRent").value);
+var Others = parseFloat(document.getElementById("other").value);
+
+var expenditure = officeRent+employeeSalary+houseRent+Others;
+var Balance = income - expenditure;
+document.getElementById("demo").innerHTML = expenditure;
+document.getElementById("balace").innerHTML = Balance;
+
+var sav =parseFloat(document.getElementById("saving").value);
+var sving = (Balance*(sav/100));
+var save = Balance-sving;
+document.getElementById("save").innerHTML = sving;
+document.getElementById("rBalance").innerHTML = save;
+
+
+if(income<0){
+    document.getElementById("demo").innerHTML = "negetive value not accepted";
+    return false;
+}
+}
+
+
+
+
+
+
