eeb224fcd01c9c70c99f09dedb37af38befcb37b
ahosan_habib <ahosanhabib310@gmail.com>
2024-01-09 02:10:59 +0600

    design changes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+var loader = document.querySelector(".loader");
+setTimeout(function(){
+    loader.style.top = "-100%"
+},4000);
 
 function calculate() {
     var income = parseFloat(document.getElementById("inc").value);
@@ -11,8 +15,10 @@ function calculate() {
     document.getElementById("demo").innerHTML = expenditure;
     document.getElementById("balace").innerHTML = Balance;
     if (income < 0) {
+        
         document.getElementById("demo").innerHTML = "negetive value not accepted";
         return false;
+        
     }
 }
 
